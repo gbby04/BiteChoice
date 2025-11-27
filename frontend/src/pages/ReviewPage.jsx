7405fb9a8ff723197ea99a55a5147c948f333876
Gabriela <gabriela.barbara9394@gmail.com>
2025-11-28 01:46:00 +0800

    Add files via upload

---
+++ frontend/src/pages/ReviewPage.jsx
@@ -0,0 +1,10 @@
+// Template for ALL missing pages
+import React from 'react';
+
+export default function ReviewPage() { // Change function name for each file!
+    return (
+        <div className="p-10 text-center text-xl font-bold">
+            Page Loaded! Content Coming Soon.
+        </div>
+    );
+}
