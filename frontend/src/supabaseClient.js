b804c7a387ac093b106b5349db747a2dab6b160a
Gabriela <gabriela.barbara9394@gmail.com>
2025-12-10 03:19:26 +0800

    Update supabaseClient.js

--- frontend/src/supabaseClient.js
+++ frontend/src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://vmkihbwdtjdookdrlibf.supabase.co' 
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZta2loYndkdGpkb29rZHJsaWJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4NDUxMTMsImV4cCI6MjA4MDQyMTExM30.uZ9f0utHXw1IGtqdXPIPL2iwSMxwrJBFjlY8HokS3Uo'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
