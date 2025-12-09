ae3b603cd75e743b738421c277ece8d3307e2980
Gabriela <gabriela.barbara9394@gmail.com>
2025-12-10 03:47:38 +0800

    Update LoginPage.jsx

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { supabase } from '../db'; // <--- Import Supabase
+import { supabase } from '../supabaseClient'; // <--- Import Supabase
 
 // --- Eye Icon Component ---
 const EyeIcon = ({ isVisible }) => (
@@ -198,3 +198,4 @@ export default function LoginPage() {
     );
 
 }
+
